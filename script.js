db60d25eade612e178d1271aa63945c980bd6d1a
Heather Runyon <hrocco25@gmail.COM>
2019-12-09 15:37:13 -0700

    change pic on question one and keep track of whats is correct or incorrect

--- script.js
+++ script.js
@@ -14,16 +14,15 @@
 const incorrect = document.querySelector('.incorrect')
 const correct =document.querySelector('.correct')
 
-incorrect.addEventListener('click', incorrectAnswer)
+//incorrect.addEventListener('click', incorrectAnswer)
 correct.addEventListener('click', correctAnswer)
 
-function incorrectAnswer(){
-    console.log('first incorrect answer')
-}
 
-function correctAnswer(){
-    console.log('correct answer for question one')
+function correctAnswer(evt){
+    // console.log('correct answer for question one')
+    // console.log(evt.target)
     //this only works on the first .correct
+    if(evt.target.classList.contains('correct')){
     correct.style.backgroundColor = '#82ad3e'
     let response = document.querySelector('.response')
     response.textContent= 'Correct!'
@@ -32,10 +31,38 @@ function correctAnswer(){
     img.setAttribute('src', 'https://media.gettyimages.com/photos/pikes-peak-avenue-colorado-springs-picture-id92414615?b=1&k=6&m=92414615&s=612x612&w=0&h=Ik_wIFzbstuJ1gbSdb87saTejsfsPxdc_hfvT96wWds=')
     response.appendChild(img)
     img.style.width= '50%'
-    img.style.border= '.15em solid #6d744b' 
+    img.style.border= '.15em solid #6d744b'
+    }
+    console.log(evt)
+    
 }
 
-//how do i get all of the .corrects to work in one function
-// for loop?
-// loop through all the correct correct responses an 
+const questions =document.querySelector('#questions')
+questions.addEventListener('click', answer)
+function answer(evt){
+    // use evt.target to get at the class name 
+    //console.log(evt.target)
+    if(evt.target.classList.contains('correct')){
+        console.log('correct')
+        
+        
+
+    }else if(evt.target.classList.contains('incorrect')){
+        console.log('incorrect')
+    }
+}
+
+
+
+
+
+// button have an active class?
+
+//how to stop it from doing multiple clicks on the button
+//if background color is not green then change it to green 
+//
+
+
+
+
 
