f84c073bf66236c7a4aa9504ed19112bc993a523
Heather Runyon <hrocco25@gmail.COM>
2019-12-10 15:56:07 -0700

    html update classes and formating

--- script.js
+++ script.js
@@ -17,54 +17,33 @@
 //and change the previous question display to none 
 // if two is showing go to three
 // 
-const questions = document.querySelector('#questions')
-console.log(questions)
-const scoreStart = 0
+
+const score = 0
 const incorrect = document.querySelectorAll('.incorrect')
 const correct =document.querySelectorAll('.correct')
 const response= document.querySelectorAll('.response')
-
 const start= document.querySelector('.start')
 let option = document.querySelectorAll('.option')
-
 const q = document.querySelectorAll('.q')
-console.log(q)
-//console.log(option)
-
+//console.log(q)
 let currentQIndex = 0
 let previousQIndex = 0
 
+//if correct add one to score
+//else do nothing 
 
-// option.addEventListener('click', startTrivia)
 
-// for(let i=0; i<option.length; i++){
-// }
+
 start.addEventListener('click', startTrivia)
+function startTrivia(){
+    q[currentQIndex].style.display = 'block' //this will make the first question load
+    console.log('current', q[currentQIndex])
+}
+//console.log(q)
 
 option.forEach(buttons =>{
     buttons.addEventListener('click', next)
 })
-
-function startTrivia(){
-    // console.log('start')
-    // previousQIndex += currentQIndex
-    // q[previousQIndex].style.display= 'none'
-    // console.log('previous', q[previousQIndex])
-// i would delte this^^^^^
-
-    q[currentQIndex].style.display = 'block' //this will make the first question load
-
-    console.log('current', q[currentQIndex])
-    
-    // option = document.querySelectorAll('.option')
-    // console.log(currentQIndex)
-    // console.log(option[currentQIndex += 1])
-    // option.addEventListener('click', startTrivia)
-    
-   
-}
-console.log(q)
-
 function next(evt){
     evt.preventDefault()
     console.log('this is next')
@@ -75,33 +54,33 @@ function next(evt){
     q[previousQIndex].style.display = 'none'
     q[currentQIndex].style.display = 'block'
    
-    // q[currentQIndex]+1
-    // q[currentQIndex].style.display='block'
-}
-// next()
-// function that make the next one appear.
-    // add to index
-    // wait for options to be clicked and make 
-    // q[with the next index ] = block
-
-
-for(let i=0; i<correct.length; i++){
-    correct[i].addEventListener('click', correctAnswer)
-    //console.log('correct') 
-
-}
-function correctAnswer(evt){
-    //console.log('evt target', evt.target)
-    evt.target.style.backgroundColor = '#82ad3e'
 }
 
-for(let i = 0; i<incorrect.length; i++){
-    incorrect[i].addEventListener('click', incorrectAnswer)
-}
-function incorrectAnswer(evt){
-    //console.log('evt for incorrect', evt)
-    evt.target.style.backgroundColor ='#F7A89D' 
-}
+
+
+
+
+
+
+// this is not doing anything at the moment
+
+// for(let i=0; i<correct.length; i++){
+//     correct[i].addEventListener('click', correctAnswer)
+//     //console.log('correct') 
+
+// }
+// function correctAnswer(evt){
+//     //console.log('evt target', evt.target)
+//     evt.target.style.backgroundColor = '#82ad3e'
+// }
+
+// for(let i = 0; i<incorrect.length; i++){
+//     incorrect[i].addEventListener('click', incorrectAnswer)
+// }
+// function incorrectAnswer(evt){
+//     //console.log('evt for incorrect', evt)
+//     evt.target.style.backgroundColor ='#F7A89D' 
+// }
     
 
 
