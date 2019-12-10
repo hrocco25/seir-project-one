d5be6c52046b765d3bcf4d81d27195badfd4384c
Heather Runyon <hrocco25@gmail.COM>
2019-12-10 15:25:21 -0700

    carousel to make only one question appear at a time

--- script.js
+++ script.js
@@ -17,34 +17,72 @@
 //and change the previous question display to none 
 // if two is showing go to three
 // 
-
+const questions = document.querySelector('#questions')
+console.log(questions)
+const scoreStart = 0
 const incorrect = document.querySelectorAll('.incorrect')
 const correct =document.querySelectorAll('.correct')
 const response= document.querySelectorAll('.response')
-// const next=document.querySelector('.next')
-// const start= document.querySelector('.start')
-const option = document.querySelector('.option')
-const one = document.querySelector('.one')
-// const two = document.querySelector('.two')
+
+const start= document.querySelector('.start')
+let option = document.querySelectorAll('.option')
+
 const q = document.querySelectorAll('.q')
 console.log(q)
+//console.log(option)
 
 let currentQIndex = 0
 let previousQIndex = 0
 
 
-option.addEventListener('click', startTrivia)
+// option.addEventListener('click', startTrivia)
+
+// for(let i=0; i<option.length; i++){
+// }
+start.addEventListener('click', startTrivia)
+
+option.forEach(buttons =>{
+    buttons.addEventListener('click', next)
+})
 
 function startTrivia(){
-    console.log('start')
-    previousQIndex += currentQIndex
-    q[previousQIndex].style.display= 'none'
-    q[currentQIndex].style.display = 'block'
+    // console.log('start')
+    // previousQIndex += currentQIndex
+    // q[previousQIndex].style.display= 'none'
+    // console.log('previous', q[previousQIndex])
+// i would delte this^^^^^
+
+    q[currentQIndex].style.display = 'block' //this will make the first question load
+
+    console.log('current', q[currentQIndex])
     
+    // option = document.querySelectorAll('.option')
+    // console.log(currentQIndex)
+    // console.log(option[currentQIndex += 1])
+    // option.addEventListener('click', startTrivia)
     
    
 }
+console.log(q)
 
+function next(evt){
+    evt.preventDefault()
+    console.log('this is next')
+    previousQIndex = currentQIndex
+    currentQIndex++
+    console.log(q[currentQIndex])
+    //q[previousQIndex].style.display= 'none'// removes last question
+    q[previousQIndex].style.display = 'none'
+    q[currentQIndex].style.display = 'block'
+   
+    // q[currentQIndex]+1
+    // q[currentQIndex].style.display='block'
+}
+// next()
+// function that make the next one appear.
+    // add to index
+    // wait for options to be clicked and make 
+    // q[with the next index ] = block
 
 
 for(let i=0; i<correct.length; i++){
@@ -52,21 +90,19 @@ for(let i=0; i<correct.length; i++){
     //console.log('correct') 
 
 }
+function correctAnswer(evt){
+    //console.log('evt target', evt.target)
+    evt.target.style.backgroundColor = '#82ad3e'
+}
 
 for(let i = 0; i<incorrect.length; i++){
     incorrect[i].addEventListener('click', incorrectAnswer)
 }
-  
-
 function incorrectAnswer(evt){
-    console.log('evt for incorrect', evt)
+    //console.log('evt for incorrect', evt)
     evt.target.style.backgroundColor ='#F7A89D' 
 }
     
-function correctAnswer(evt){
-    console.log('evt target', evt.target)
-    evt.target.style.backgroundColor = '#82ad3e'
-}
 
 
 
